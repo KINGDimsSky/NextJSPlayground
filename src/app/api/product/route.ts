73edd4cddfd1e3453b535b1407d8f38ea80c36c5
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-03-23 05:51:08 +0700

    Fetching NextJS

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const data = [
-    {id : 1, name: "Sepatu Baru", price: 100000},
-    {id : 2, name: "Sepatu Lama", price: 250000}
+    {id : 1, title: "Sepatu Baru", price: 100000, image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d130fcfa-7169-4172-8955-bf39cc544527/NIKE+VOMERO+18.png"},
+    {id : 2, title: "Sepatu Lama", price: 250000, image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d130fcfa-7169-4172-8955-bf39cc544527/NIKE+VOMERO+18.png"}
 ]
 
 export async function GET(request: NextRequest) {
--- src/app/products/[[...slug]]/page.tsx
+++ src/app/products/[[...slug]]/page.tsx
@@ -4,12 +4,38 @@ interface PropsPage {
     }
 }
 
-export default function DetailProduct ({params} : PropsPage) {
+async function getData() {
+    /* const res = await fetch('https://fakestoreapi.com/products'); */
+    const res = await fetch('http://localhost:3000/api/product')
+    if (!res.ok) {
+        throw new Error('Failed To Fetch Data')
+    }
+    return res.json();
+}
+
+export default async function ProductPage ({params} : PropsPage) {
     const {slug} = params
+    const products = await getData()
 
     return (
-        <div>
-            <h1>{slug ? "Detail Products Page" : "Products Page"}</h1>
+        <div className="grid grid-cols-4 gap-2 mt-5 items-center">
+            {/* <h1>{slug ? "Detail Products Page" : "Products Page"}</h1> */}
+            {products.data.length > 0 && products.data.map((product: any) => (
+              <div key={product.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 my-5">
+                    <a href="#">
+                        <img className="p-8 rounded-t-lg object-cover h-96 w-full" src={product.image} alt="product image" />
+                    </a>
+                    <div className="px-5 pb-5">
+                        <a href="#">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">{product.title}</h5>
+                        </a>
+                        <div className="flex items-center justify-between mt-3">
+                            <span className="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
+                            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                        </div>
+                    </div>
+              </div>
+            ))}
             {slug && (<>
                 <h1>Detail Products</h1>
                 <h1>Category : {slug[0]}</h1>
