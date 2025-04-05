a52a565c7e4011703c18d02fc4fb2805ec6466cb
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-04-06 05:06:59 +0700

    Learning NextAuth

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,11 +1,14 @@
 'use client'
 
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function Navbar() {
     const pathname = usePathname();
-    const router = useRouter()
+    const router = useRouter();
+    const {data: session, status } = useSession()
+
     return (
         <div className="bg-black flex items-center justify-between py-2 px-5 z-50 mb-4">
             <div className="flex items-center gap-2 ml-6">
@@ -14,7 +17,14 @@ export default function Navbar() {
                 <Link href="/about"><p className={`${pathname === '/about' ? 'text-blue-300' : 'text-white'}`}>About</p></Link>
                 <Link href="/about/profile"><p className={`${pathname === '/about/profile' ? 'text-blue-300' : 'text-white'}`}>Profile</p></Link>
             </div>
-            <button onClick={() => router.push('/login')} className="bg-white text-sm rounded-md px-3 cursor-pointer">Login</button>
+            {status === 'loading' ? (
+                <p>Loading ...</p>
+            ): (session?.user ? (
+                <button onClick={() => router.push('/login')} className="bg-white text-sm rounded-md px-3 cursor-pointer">LogOut</button>
+            ): (
+                <button onClick={() => router.push('/login')} className="bg-white text-sm rounded-md px-3 cursor-pointer">Login</button>
+            ))}
+           
         </div>
     )
 }
